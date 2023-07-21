36a274595ad59f5e54d2dd72b9f68c6c3154787d
AmmadLala <139677523+AmmadLala@users.noreply.github.com>
2023-07-21 11:26:22 +0500

    Question 2

--- index.ts
+++ index.ts
@@ -1,5 +1,3 @@
-const anyname : string = "MuhaMmad ammAd";
-console.log(`Print Same name : ${anyname}`);
-console.log(`Print uppercase name:${anyname.toUpperCase()}`);
-console.log(`Print lowecase name:${anyname.toLowerCase()}`);
-console.log(`Print Titlecase name:${anyname.replace(/\w\S*/g,(txt)=>txt.charAt(0).toUpperCase()+txt.substring(1).toLowerCase())}`);
+const anyname : string = "Ammad" ;
+const msg : string =`Hello ${anyname}, this is my first step towards programming`;
+console.log(msg);
