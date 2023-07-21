cab4b0e363dd2c2fa525f97925bcc06be7a85b1e
AmmadLala <139677523+AmmadLala@users.noreply.github.com>
2023-07-21 11:16:47 +0500

    Question 2

---
+++ index.js
@@ -0,0 +1,4 @@
+"use strict";
+const anyname = "Ammad";
+const msg = `Hello ${anyname}, this is my first step towards programming`;
+console.log(msg);
---
+++ index.ts
@@ -0,0 +1,3 @@
+const anyname : string = "Ammad" ;
+const msg : string =`Hello ${anyname}, this is my first step towards programming`;
+console.log(msg);
